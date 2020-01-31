22edd1644b53396050b5a22d371b793607548245
fadimata tillman <fadi.tillman@gmail.com>
2020-01-31 03:35:34 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,8 @@
-def my_each ()# put argument(s) here
-  # def hello_t(array)
-  if block_given?
-    i = 0
+def my_each (array)
+  i = 0
 
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
-
-    array
-  else
-    puts "Hey! No block was given!"
+  while i < array.length
+    yield array[i]
+    i = i + 1
   end
 end
